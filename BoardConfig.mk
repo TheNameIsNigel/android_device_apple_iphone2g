b99acd5bd43fa9d4984c7e184461917928c53e46
Ryan Norris <nigeluno@norrisenterprises.org>
2013-06-17 18:39:58 -0400

    Hello. :P

---
+++ BoardConfig.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(dir $(lastword $(MAKEFILE_LIST)))
+
+USE_CAMERA_STUB := true
+
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+#WPA_SUPPLICANT_VERSION      := VER_0_6_X
+#BOARD_WLAN_DEVICE           := libertas_sdio
+#WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/libertas_sdio.ko"
+#WIFI_DRIVER_MODULE_ARG      := ""
+#WIFI_DRIVER_MODULE_NAME     := "libertas_sdio"
+
+COMMON := $(LOCAL_PATH)/../common
+include $(COMMON)/BoardConfigCommon.mk
